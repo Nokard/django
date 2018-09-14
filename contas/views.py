58d1f8fb91216d5c75215071f4fb1abdd2d076eb
rodrigueshugodias <rodrigueshugodias@hotmail.com>
2018-09-14 11:11:30 -0300

    Projeto DJANGO Udemy

---
+++ contas/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1','t2','t2']
+    data['dinheiro'] = ['a1','a2','a3']
+
+    data['now'] = datetime.datetime.now()
+
+    return render(request, 'contas/home.html', data)
