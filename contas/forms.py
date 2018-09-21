62f8abd21a86defb8a2d649a3f52b139034246c7
rodrigueshugodias <rodrigueshugodias@hotmail.com>
2018-09-21 17:55:01 -0300

    tudo

--- contas/forms.py
+++ contas/forms.py
@@ -3,7 +3,7 @@ from .models import Transacao, Categoria, Clientes
 from django import forms
 from django.contrib.auth.decorators import login_required
 from django import forms
-
+from django.contrib.auth import authenticate
 
 
 class TransacaoForm(ModelForm):
@@ -31,7 +31,20 @@ class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+    #Essa funcao limpa os campos username e password
     def clean(self, *args, **kwargs):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
+
+        if username and password:
+            #para usar o authentication precisa importar from django.contrib.auth import authenticate
+            user = authenticate(username=username,  password=password)
+            if not user:
+                raise forms.ValidationError('Esse usuario não existe')
+            if not user.check_password(password):
+                raise forms.ValidationError('Senha errada')
+            if not user.is_activate:
+                raise forms.ValidationError('Nao esta autenticacdo')
+        return super()
+
 
